494ad08d99dc2592dda18d0c0c65fca83587f296
hiro28 <cuongnhse90131@fpt.edu.vn Primary Public cuongnhse90131@fpt.edu.vn Primary Public cuongnhse90131@fpt.edu.vn>
2016-11-06 14:49:08 +0700

    Initial Commit

---
+++ ImageProDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageProDownloader
+//
+//  Created by Cuong Nguyen on 06/11/2016.
+//  Copyright © 2016 Cuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
